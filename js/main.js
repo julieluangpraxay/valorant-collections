1205dff32276d8729cb93690c0fc68c847c0cf17
julieluangpraxay <julieluangpraxay@gmail.com>
2023-10-06 17:28:22 +0000

    Added the renderSprays function, viewswap, and domcontent loaded. Still working on getting the favorites button to work properly

--- js/main.js
+++ js/main.js
@@ -159,3 +159,86 @@ const $close = document.querySelector('.fa-xmark');
 $close.addEventListener('click', function (event) {
   $mobile.classList.add('hidden');
 });
+
+function renderSprays(sprays) {
+  const searchTerm = $searchBar.value.toLowerCase();
+  $cardContainer.innerHTML = ' ';
+  for (let i = 0; i < data.sprays.length; i++) {
+    const spray = data.sprays[i];
+    const sprayName = spray.displayName.toLowerCase();
+
+    if (searchTerm === '' || (sprayName.includes(searchTerm) && spray.fullTransparentIcon !== null)) {
+      const $cardWrapper = document.createElement('div');
+      const $iconImage = document.createElement('img');
+      const $textWrapper = document.createElement('div');
+      const $sprayTitle = document.createElement('p');
+      const $imgWrapper = document.createElement('div');
+      const $imgRow = document.createElement('div');
+
+      $sprayTitle.textContent = spray.displayName;
+      $iconImage.src = spray.animationGif || spray.fullTransparentIcon;
+      $iconImage.setAttribute('alt', 'Valorant Spray Image');
+
+      const $heart = document.createElement('i');
+      if (data.favorites.includes(spray.displayName)) {
+        $heart.classList.add('fa-solid', 'fa-heart', 'fa-2xl', 'image-heart');
+      } else {
+        $heart.classList.add('fa-regular', 'fa-heart', 'fa-2xl', 'image-heart');
+      }
+      $imgRow.classList.add('img-row');
+      $imgWrapper.classList.add('img-wrapper');
+      $cardWrapper.classList.add('card-wrapper');
+      $iconImage.classList.add('spray-icon');
+      $textWrapper.classList.add('text-wrapper');
+      $sprayTitle.classList.add('spray-title');
+
+      $cardContainer.appendChild($cardWrapper);
+      $cardWrapper.appendChild($imgWrapper);
+      $imgWrapper.appendChild($imgRow);
+      $imgRow.appendChild($heart);
+      $imgWrapper.appendChild($iconImage);
+      $cardWrapper.appendChild($textWrapper);
+      $textWrapper.appendChild($sprayTitle);
+
+      $heart.addEventListener('click', function (event) {
+        $heart.classList.remove('fa-regular');
+        $heart.classList.add('fa-solid', 'fa-heart');
+
+        const clickedHeart = event.target;
+        const cardContainer = clickedHeart.closest('.card-wrapper');
+        const closestSprayName = cardContainer.lastChild.firstChild.textContent;
+        data.favorites.push(closestSprayName);
+
+        return $cardWrapper;
+      }
+      );
+    }
+  }
+}
+
+const $favoritesPage = document.querySelector('.favorites-page');
+const $spraysPage = document.querySelector('.sprays-page');
+const $favoritesNavButton = document.querySelector('.favorite-nav-button');
+
+$favoritesNavButton.addEventListener('click', function (event) {
+
+  viewSwap();
+}
+
+);
+
+const $ul = document.querySelector('ul');
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.favorites.length; i++) {
+    $ul.append(renderSprays(data.favorites[i]));
+  }
+  viewSwap(data.favorites);
+});
+
+function viewSwap(viewName) {
+  if (viewName === 'sprays-page') {
+    data.view = 'favorites-page';
+    $favoritesPage.classList.remove('hidden');
+    $spraysPage.classList.add('hidden');
+  }
+}
